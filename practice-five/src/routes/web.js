fafde901c7bc301cbcd622d6083cc62eaf16af9a
EduardoMVirgilio <eduardomiguelvirgilio@gmail.com>
2020-07-18 22:22:20 -0300

    FInal de las practicas

--- practice-five/src/routes/web.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router()
-
-const controller = require("../controllers/web")
-
-router.get("/",controller.index)
-
-router.get("/nosotros",controller.nosotros)
-
-module.exports = router
--- practice-six/src/models/task.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-const path = require("path")
-module.exports = {
-    all: () => JSON.parse(fs.readFileSync(path.resolve(__dirname,"../data/task.json"),"utf-8")),
-    one: (id) => JSON.parse(fs.readFileSync(path.resolve(__dirname,"../data/task.json"),"utf-8")).find(element => element.id == id)
-}
